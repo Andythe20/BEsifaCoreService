9693bd2a55371ac5ab65fed975ba583e2320153a
Andrés Ortega <andythe20xd@gmail.com>
2026-04-15 17:38:36 -0400

    Levantamiento de proyecto core-sifa con java 21

---
+++ src/main/java/com/example/core_sifa/CoreSifaApplication.java
@@ -0,0 +1,13 @@
+package com.example.core_sifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSifaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSifaApplication.class, args);
+	}
+
+}
