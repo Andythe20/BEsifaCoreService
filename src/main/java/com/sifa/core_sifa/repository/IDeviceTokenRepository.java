60386e5c656664a8c75664417f81a50dac1f0eea
Jarod Smith <lbriones.dev@gmail.com>
2026-06-08 00:02:30 -0400

    feat: agregar entidad DeviceToken y su repositorio

---
+++ src/main/java/com/sifa/core_sifa/repository/IDeviceTokenRepository.java
@@ -0,0 +1,16 @@
+package com.sifa.core_sifa.repository;
+
+import com.sifa.core_sifa.model.DeviceToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface IDeviceTokenRepository extends JpaRepository<DeviceToken, Long> {
+
+    Optional<DeviceToken> findByToken(String token);
+
+    List<DeviceToken> findByAppVersionNot(String appVersion);
+}
